1d17df192ab6087310c40a2e5c6be1af9cf37f22
Brian Shaw <1BrianShaw@gmail.com>
2023-03-20 09:06:11 -0400

    1st pass

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    class OpenWeatherMapAPI
     {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var apiKey = "64ab2cb3ebfb34e5ef9245fea3413214";
+            Console.WriteLine("Enter in a city: ");
+            var cityName = Console.ReadLine();
+
+            var weatherMapURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+            string weatherResponse = client.GetStringAsync(weatherMapURL).Result;
+            JObject weatherObject = JObject.Parse(weatherResponse);
+
+            Console.WriteLine("Weather Object");
+
+            
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace APIsAndJSON
-{
-    public class Program
-    {
-        static void Main(string[] args)
+﻿using Newtonsoft.Json.Linq;
+
+
+
+
+
+        HttpClient client = new HttpClient();
+        
+        for (int i = 1; i <= 5; i++)
         {
-            Console.WriteLine("Hello, World!");
+           string kanyeUrl = "https://api.kanye.rest/";
+
+           string KanyeResponse = client.GetStringAsync(kanyeUrl).Result;
+
+           JObject kanyeQuote = JObject.Parse(KanyeResponse);
+            
+            Console.WriteLine($"------------");
+            Console.WriteLine($"Kanye:{kanyeQuote["quote"]}");
+            Console.WriteLine($"------------");
+        
+            string ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronUrl).Result;
+
+            var ronQuote = JArray.Parse(ronResponse);
+
+            Console.WriteLine($"------------");
+            Console.WriteLine($"Ron:{ronQuote[0]}");
+            Console.WriteLine($"------------");
+
         }
-    }
-}
+   
